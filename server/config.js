dc4e228eb237b1a646cfad3ce34f998040ef1b5f
bryanyang <bryanyang@gmail.com>
2024-01-25 10:39:28 +0800

    fix

--- server/config.js
+++ server/config.js
@@ -9,70 +9,42 @@
 const prizes = [
   {
     type: 0,
-    count: 1000,
+    count: 5,
     title: "",
     text: "特别奖"
   },
   {
-    type: 1,
-    count: 2,
-    text: "特等奖",
-    title: "神秘大礼",
+    type: 2,
+    count: 34,
+    text: "一等奖",
+    title: "",
     img: "../img/secrit.jpg"
   },
   {
-    type: 2,
-    count: 5,
-    text: "一等奖",
-    title: "Mac Pro",
-    img: "../img/mbp.jpg"
-  },
-  {
     type: 3,
-    count: 6,
+    count: 50,
     text: "二等奖",
-    title: "华为 Mate30",
-    img: "../img/huawei.png"
+    title: "",
+    img: "../img/secrit.jpg"
   },
   {
     type: 4,
-    count: 7,
+    count: 60,
     text: "三等奖",
-    title: "Ipad Mini5",
-    img: "../img/ipad.jpg"
+    title: "",
+    img: "../img/secrit.jpg"
   },
-  {
-    type: 5,
-    count: 8,
-    text: "四等奖",
-    title: "大疆无人机",
-    img: "../img/spark.jpg"
-  },
-  {
-    type: 6,
-    count: 8,
-    text: "五等奖",
-    title: "Kindle",
-    img: "../img/kindle.jpg"
-  },
-  {
-    type: 7,
-    count: 11,
-    text: "六等奖",
-    title: "漫步者蓝牙耳机",
-    img: "../img/edifier.jpg"
-  }
 ];
 
 /**
  * 一次抽取的奖品个数与prizes对应
  */
-const EACH_COUNT = [1, 1, 5, 6, 7, 8, 9, 10];
+const EACH_COUNT = [5, 30, 40, 40];
 
 /**
  * 卡片公司名称标识
  */
-const COMPANY = "MoShang";
+const COMPANY = "新华";
 
 module.exports = {
   prizes,
